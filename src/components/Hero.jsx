55fce246ff303d9d35ffbe48b723fc47b78c22fe
Danut Grigore <voicuion488@gmail.com>
2025-04-09 19:35:52 +0100

    Added new profile photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import profileImage from '../assets/profile5.png';
+import profileImage from '../assets/profile1.png';
 
 const Hero = () => {
   return (
